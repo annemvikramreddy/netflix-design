30f50c528019f92593288fdb19971bc994764075
annemvikramreddy <annemvikramreddy@gmail.com>
2024-06-25 16:50:53 -0400

    basic code

---
+++ src/config.js
@@ -0,0 +1,22 @@
+// replace below firebase config with your own
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMDSAvDDO4gdPRg8yVeujplW14YOP58mM",
+  authDomain: "klims-fefb-befb-dev-7b4a7.firebaseapp.com",
+  projectId: "klims-fefb-befb-dev-7b4a7",
+  storageBucket: "klims-fefb-befb-dev-7b4a7.appspot.com",
+  messagingSenderId: "1027067126001",
+  appId: "1:1027067126001:web:3588910f8a680d45f58950",
+  measurementId: "G-S1KLHWM76M"
+};
+
+
+
+
+// craete a/c on themoviedb.org and replace access token below
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMWYxMGFmZDYyZjY1NTU5ZjU4ODBlNmU5MTQ2YWZkYyIsInN1YiI6IjYxMDdhMTM1NzcxOWQ3MDAyYzllMDQ0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iNtZikm1EQz-8P2gQrN-3LdmyBmtcvD-QOh7EY38Bh4";
+
+
+  export {firebaseConfig, TMDB_Access_Key};
